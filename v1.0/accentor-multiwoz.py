dd2e710f93d025f5e9cca6952e338491b70ba61d
Kai Sun <sunkaicn@gmail.com>
2020-11-02 23:38:34 -0500

    data v1

---
+++ v1.0/accentor-multiwoz.py
@@ -0,0 +1,25 @@
+import json
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--source", default="./MultiWOZ_2.1/data.json", type=str, required=False, help="Path to the MultiWOZ dataset.")
+    args = parser.parse_args()
+    
+    with open("candidates-multiwoz.json", "r", encoding='utf8') as f:
+        augmentation = json.load(f)
+
+    with open(args.source, "r", encoding='utf8') as f:
+        data = json.load(f)
+
+    data = {x:data[x] for x in data if x in augmentation}
+
+    for x in data:
+        for i in range(1, len(data[x]["log"]), 2):
+            data[x]["log"][i]["beginning"] = []
+            data[x]["log"][i]["end"] = []
+        for cc in augmentation[x]:
+            data[x]["log"][cc[0]][cc[1]] += [{"candidate": cc[2], "justification": cc[3]}]
+
+    with open("accentor-multiwoz-1k.json", "w", encoding='utf8') as f:
+        json.dump(data, f, indent=1, ensure_ascii=False)
---
+++ v1.0/accentor-sgd.py
@@ -0,0 +1,34 @@
+import json
+import argparse
+import os
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--source", default="./dstc8-schema-guided-dialogue", type=str, required=False, help="Path to the SGD dataset.")
+    parser.add_argument("--target", default="./accentor-sgd", type=str, required=False, help="The target directory to store ACCENTOR-SGD.")
+    args = parser.parse_args()
+
+    with open("candidates-sgd.json", "r", encoding='utf8') as f:
+        augmentation = json.load(f)
+
+    for subdir in ["train", "dev", "test"]:
+        targetdir = os.path.join(args.target, subdir)
+        sourcedir = os.path.join(args.source, subdir)
+        os.makedirs(targetdir, exist_ok=True)
+        fns = os.listdir(sourcedir)
+        for fn in fns:
+            if not fn.endswith(".json"):
+                continue
+            with open(os.path.join(sourcedir, fn), "r", encoding='utf8') as f:
+                data = json.load(f)
+            if fn.startswith("dialogue"):
+                for i in range(len(data)):
+                    for j in range(1, len(data[i]["turns"]), 2):
+                        data[i]["turns"][j]["beginning"] = []
+                        data[i]["turns"][j]["end"] = []
+                    for cc in augmentation[subdir + data[i]["dialogue_id"]]:
+                        data[i]["turns"][cc[0]][cc[1]] += [{"candidate": cc[2], "justification": cc[3]}]
+            with open(os.path.join(targetdir, fn), "w", encoding='utf8') as f:
+                json.dump(data, f, indent=1, ensure_ascii=False)
+            
+            
